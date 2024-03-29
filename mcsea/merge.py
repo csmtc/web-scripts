d1c5bc677d043371e65172699ed802bd720d7a75
csmtc <tackwork@outlook.com>
2024-03-29 17:00:47 +0800

    modify

---
+++ mcsea/merge.py
@@ -0,0 +1,41 @@
+from typing import Iterable, List
+import os
+import re
+from datetime import datetime
+
+DATE_REGEX = re.compile(r"^(20\d{2}-\d{1,2}-\d{1,2}) (.*)")
+MERGE_PREFIX = "SERIES-"
+
+
+def mergeFiles(file_paths: Iterable[str], dst_dir: str = None):
+    if dst_dir is None:
+        dst_dir, dst_fname = os.path.split(file_paths[-1])
+    else:
+        dst_fname = os.path.split(file_paths[-1])[-1]
+    match_result = re.match(DATE_REGEX, dst_fname)
+    if match_result is not None:
+        dst_fname = match_result.group(2)
+        lastPostTime = match_result
+    else:
+        lastPostTime = datetime.now().strftime(r"%Y-%m-%d")
+    dst_fname, fext = os.path.splitext(dst_fname)
+    dst_fname = MERGE_PREFIX + dst_fname + "-Utl" + lastPostTime + fext
+    print("Destination file:", dst_fname)
+    ctxs = ""
+    for fpath in file_paths:
+        src_fname = os.path.split(fpath)[-1]
+        if src_fname.startswith(MERGE_PREFIX):
+            continue
+        print(src_fname)
+        with open(fpath, "r", encoding="utf-8") as f:
+            ctxs = ctxs + f.read() + "\n\n\n"
+    with open(os.path.join(dst_dir, dst_fname), "w", encoding="utf-8") as f:
+        f.write(ctxs)
+
+
+if __name__ == "__main__":
+    dir = "/storage/emulated/0/Documents/merge"
+    files = os.listdir(dir)
+    files.sort(key=lambda fname: int(re.search(r"(\d{1,2})", fname).group(1)))
+    files = [os.path.join(dir, f) for f in files]
+    mergeFiles(files)
