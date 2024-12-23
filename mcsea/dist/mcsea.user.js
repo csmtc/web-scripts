4682a8a4f8ee7ee4926f20b48f11e98eb7b30165
csmtc <csmtc@outlook.com>
2024-12-23 16:26:57 +0800

    12.23commit

--- mcsea/dist/mcsea.user.js
+++ mcsea/dist/mcsea.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         mcsea
 // @namespace    https://mcseas.club/
-// @version      2024.12.22
+// @version      2024.12.23
 // @author       monkey
 // @description  prettify and download novel on mcsea
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mcseas.club
